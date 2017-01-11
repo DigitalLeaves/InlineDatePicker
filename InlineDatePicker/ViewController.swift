f50c9557f838b28c0b39a5435edf831c07ecc84b
Ignacio Nieto <contact@digitalleaves.com>
2017-01-11 15:48:34 +0100

    Initial Commit

---
+++ InlineDatePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InlineDatePicker
+//
+//  Created by Ignacio Nieto Carvajal on 11/1/17.
+//  Copyright © 2017 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
